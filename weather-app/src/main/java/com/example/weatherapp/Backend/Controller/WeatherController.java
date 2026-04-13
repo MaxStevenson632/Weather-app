ee4b3116fe0524f805651eefa42642b7401c0d63
MaxStevenson632 <max.stevenson5@outlook.com>
2026-04-13 19:31:54 +0100

    Completed Controller class

---
+++ weather-app/src/main/java/com/example/weatherapp/Backend/Controller/WeatherController.java
@@ -0,0 +1,30 @@
+package com.example.weatherapp.Backend.Controller;
+
+import com.example.weatherapp.Backend.Models.CurrentWeatherResponse;
+import com.example.weatherapp.Backend.Models.ForecastWeatherResponse;
+import com.example.weatherapp.Backend.Service.WeatherService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:63342")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    public record WeatherDashboard(CurrentWeatherResponse current, ForecastWeatherResponse forecast) {}
+
+    @GetMapping("/weather")
+    public WeatherDashboard getCurrentAndForecast(@RequestParam String location) {
+        CurrentWeatherResponse current = weatherService.getCurrentWeather(location);
+        ForecastWeatherResponse forecast = weatherService.getForecast(location);
+
+        return new WeatherDashboard(current, forecast);
+    }
+}
