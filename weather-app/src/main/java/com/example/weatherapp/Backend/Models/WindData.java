78970224fa1be7a5be3fadc564c9edae5ad857b8
MaxStevenson632 <max.stevenson5@outlook.com>
2026-04-13 19:30:36 +0100

    Completed DTO for WeatherData, MainData and WindData responses

---
+++ weather-app/src/main/java/com/example/weatherapp/Backend/Models/WindData.java
@@ -0,0 +1,14 @@
+package com.example.weatherapp.Backend.Models;
+
+public class WindData {
+
+    private float speed;
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+}
